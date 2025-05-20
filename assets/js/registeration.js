0027b124c6e0adcc675a8090aa7a4cf0c70fc489
orji Dominion <orjidominion32@gmail.com>
2025-05-20 15:27:35 +0100

    70% filo

--- assets/js/registeration.js
+++ assets/js/registeration.js
@@ -1,27 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById('enrollment-form');
-
-  
-  
-
-
-  function validateStep() {
-    let isValid = true;
-    // const currentStepElement = steps[step];
-    const inputs = currentStepElement.querySelectorAll('input, select');
-    
-    inputs.forEach(input => {
-      if (!input.value.trim()) {
-        input.closest('.input-field').classList.add('error');
-        isValid = false;
-      } else {
-        input.closest('.input-field').classList.remove('error');
-      }
-    });
-    
-    return isValid;
-  }
-});
 
  
 
